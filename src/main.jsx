b6a05eb4b7de372178ecf77261d28411be9d6c05
SachinPadyar <hiteshgirnar99@gmail.com>
2026-07-04 14:36:56 +0530

    Updated favicons

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './utils/globalAnimationObserver'
+import faviconIco from './assets/favicon.ico'
+
+const applyFavicons = () => {
+  const faviconLinks = [
+    { rel: 'icon', type: 'image/x-icon', href: faviconIco },
+    { rel: 'shortcut icon', type: 'image/x-icon', href: faviconIco },
+  ]
+
+  faviconLinks.forEach(({ rel, type, href }) => {
+    let link = document.head.querySelector(`link[rel="${rel}"][type="${type}"]`)
+
+    if (!link) {
+      link = document.createElement('link')
+      link.rel = rel
+      link.type = type
+      document.head.appendChild(link)
+    }
+
+    link.href = href
+  })
+}
+
+applyFavicons()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
